214c55cc755bee4ddb719fa49b247cc3fc44ceff
Hunter Lopez <hunterlopez@Hunters-MacBook-Pro.local>
2018-10-10 18:39:40 -0400

    try to get ready for heroku

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
-var server = app.listen(3000, () => {
-    console.log('Server is listening on port', server.address().port)
-})
+var server = app.listen(process.env.PORT)//.listen(3000, () => {
+    //console.log('Server is listening on port', server.address().port)
+//})
